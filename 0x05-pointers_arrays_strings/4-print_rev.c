975542bb6f397fb97a92aa5b39bf7146b82b80ce
Deadpool1997 <safaryharold@gmail.com>
2022-09-19 13:33:42 +0300

    I am enjoying coding with scripts

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse followed by new line
+ * @s: input string
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	char *init = s;
+
+	if (*s != '\0')
+	{
+		while (*s != '\0')
+			s++;
+
+		--s;   /*going back before the \0 */
+		while (s != init)
+		{
+			_putchar(*s);
+			--s;
+		}
+		_putchar(*s);    /*printing when *s == init */
+	}
+	_putchar('\n');
+}
