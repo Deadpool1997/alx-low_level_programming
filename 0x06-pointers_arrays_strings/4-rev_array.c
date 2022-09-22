5f47f86b6fe3b08657473cd9860008265204cf06
Deadpool1997 <safaryharold@gmail.com>
2022-09-22 15:48:42 +0300

    now

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,40 +1,24 @@
 #include "main.h"
+
 /**
  * reverse_array - reverses the content of an array of integers
- * @a: an array of integers
+ * @a: pointer to array to be reversed
  * @n: number of elements of the array
- * Return: returns void
+ *
+ * Return: void
  */
 
 void reverse_array(int *a, int n)
-
 {
-	int *start_c, *end_c, c;
-	int i;
+	int i = 0;
 
-	/*Start_c and end_c is equal value of variable s*/
-	start_c = a;
-	end_c = a;
-
-	/*Move the end_c to the last character*/
-	for (i = 0; i < n - 1; i++)
+	--n;
+	while (i < n)
 	{
-		end_c++;
+		a[i] ^= a[n];
+		a[n] ^= a[i];
+		a[i] ^= a[n];
+		i++;
+		n--;
 	}
-
-	/* Swap the char from start and end */
-	/* index using begin_ptr and end_ptr */
-	for (i = 0; i < n / 2; i++)
-	{
-
-		/*swap character*/
-		c = *end_c;
-		*end_c = *start_c;
-		*start_c = c;
-
-		/*update pointers positions*/
-		start_c++;
-		end_c--;
-	}
-
 }
