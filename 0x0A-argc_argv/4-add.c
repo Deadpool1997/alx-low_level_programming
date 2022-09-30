2af23c5e3b73a3a5e0d18ac5f389fb31cace7ee6
Deadpool1997 <safaryharold@gmail.com>
2022-09-30 16:55:24 +0300

    correct

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,39 +1,49 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 
 /**
- * main - sum of  the args
- * @argc: Argument count
- * @argv: Array of argument strings
- *
- * Return: 0 for successful exit
- */
-int main(int argc, char *argv[])
+* main - adds numbers. checks if number
+* @argc: number of arguments
+* @argv: the vector that hold the arguments
+*
+* Description: using arc and argv to achieve the intended function
+* Return: returns 0 if it works or something else if error
+* A: if arg has no nunber inputs then print 0
+* B: if arg has some inputs loop the code via while loop, decrement argc
+* C: loop through the string of argv and check if anything not a digit
+* D: If it is not a digit then enter the error statement
+* E: add the atoi of the arg cause it passed the checks.
+*/
+
+int main(int argc, char **argv)
 {
-
-	int i = 0;
 	int sum = 0;
+	int i = 1;
+	int j;
 
-	if (argc == 1)
+	if (argc == 1)/* A */
 	{
 		printf("0\n");
+		return (0);
 	}
-	else
+	while (argc > 1)/* B */
 	{
-		for (i = 1; i < argc; i++)
+		j = 0;
+		while (argv[i][j])/* C */
 		{
-			if (!isdigit(*argv[i]))
+			if (!isdigit(argv[i][j]))/* D */
 			{
 				printf("Error\n");
+				return (1);
 			}
-			else
-			{
-				sum += atoi(argv[i]);
-			}
+			j++;
 		}
-		printf("%d\n", sum);
+		sum += atoi(argv[i]);/* E */
+		i++;
+		argc--;
 	}
-
+	printf("%i\n", sum);
 	return (0);
 }
